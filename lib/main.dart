841af5e55ec18d47d6356b9467e83e7a734ec8bb
IIvexII <mrh4ck3r1738@gmail.com>
2021-02-24 15:39:21 +0500

    Transection class is created for transection records :pencil:.

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,4 @@
+import 'package:Personal_Expense/transection.dart';
 import 'package:flutter/material.dart';
 
 void main() => runApp(PersonalExpense());
@@ -6,6 +7,7 @@ class PersonalExpense extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      debugShowCheckedModeBanner: false,
       title: 'Personal Expense',
       home: MyHomePage(),
     );
@@ -13,14 +15,43 @@ class PersonalExpense extends StatelessWidget {
 }
 
 class MyHomePage extends StatelessWidget {
+  final List<Transection> transections = [];
+
+  @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: Text('Personal Expenses'),
-      ),
-      body: Center(
-        child: Text('Playground!!'),
-      ),
-    );
+        appBar: AppBar(
+          title: Text('Personal Expenses'),
+        ),
+        body: Column(
+          mainAxisAlignment: MainAxisAlignment.spaceAround,
+          crossAxisAlignment: CrossAxisAlignment.stretch,
+          children: <Widget>[
+            Card(
+              color: Colors.blue,
+              child: Text(
+                'CHART',
+                style: TextStyle(
+                    color: Colors.white,
+                    fontSize: 20,
+                    fontWeight: FontWeight.w500),
+                textAlign: TextAlign.center,
+              ),
+              elevation: 20,
+            ),
+            Card(
+              color: Colors.blue,
+              child: Text(
+                'LIST OF Transections',
+                style: TextStyle(
+                    color: Colors.white,
+                    fontSize: 20,
+                    fontWeight: FontWeight.w500),
+                textAlign: TextAlign.center,
+              ),
+              elevation: 20,
+            )
+          ],
+        ));
   }
 }
