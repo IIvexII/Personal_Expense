d2f4cafac7a3696daf9340c317df93e835c3d373
IIvexII <mrh4ck3r1738@gmail.com>
2021-02-25 12:53:42 +0500

    Improved performance :hourglass_flowing_sand: by adding listView.builder()

--- lib/widgets/transectionList.dart
+++ lib/widgets/transectionList.dart
@@ -12,70 +12,71 @@ class TransectionList extends StatelessWidget {
   Widget build(BuildContext context) {
     return Container(
       height: 400,
-      child: SingleChildScrollView(
-        child: Column(
-          children: _userTransections.map((tx) {
-            return Card(
-              elevation: 5,
-              child: Row(
-                children: <Widget>[
-                  //############################################
-                  //-------------AMOUNT OF EXPENSE--------------
-                  //############################################
-                  Container(
-                    margin: EdgeInsets.all(20),
-                    padding: EdgeInsets.all(15),
-                    decoration: BoxDecoration(
-                      border: Border.all(
-                        color: Colors.purple,
-                        width: 2,
-                      ),
-                    ),
-                    child: Text(
-                      'Rs. ${tx.amount}',
-                      style: TextStyle(
-                        color: Colors.purple,
-                        fontSize: 20,
-                        fontWeight: FontWeight.bold,
-                      ),
+      child: ListView.builder(
+        itemBuilder: (ctx, index) {
+          return Card(
+            elevation: 5,
+            child: Row(
+              children: <Widget>[
+                //############################################
+                //-------------AMOUNT OF EXPENSE--------------
+                //############################################
+                Container(
+                  margin: EdgeInsets.all(20),
+                  padding: EdgeInsets.all(15),
+                  decoration: BoxDecoration(
+                    border: Border.all(
+                      color: Colors.purple,
+                      width: 2,
                     ),
                   ),
-                  //###############################################
-                  //------------END OF EXPENSE AMOUNT--------------
-                  //###############################################
-                  //----------Title and Date of expense------------
-                  //###############################################
-                  Column(
-                    crossAxisAlignment: CrossAxisAlignment.start,
-                    children: <Widget>[
-                      Container(
-                        margin: EdgeInsets.only(bottom: 10),
-                        child: Text(
-                          tx.title, // TITLE
-                          style: TextStyle(
-                            fontSize: 20,
-                            fontWeight: FontWeight.bold,
-                          ),
+                  child: Text(
+                    'Rs. ${_userTransections[index].amount}',
+                    style: TextStyle(
+                      color: Colors.purple,
+                      fontSize: 20,
+                      fontWeight: FontWeight.bold,
+                    ),
+                  ),
+                ),
+                //###############################################
+                //------------END OF EXPENSE AMOUNT--------------
+                //###############################################
+                //----------Title and Date of expense------------
+                //###############################################
+                Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: <Widget>[
+                    Container(
+                      margin: EdgeInsets.only(bottom: 10),
+                      child: Text(
+                        _userTransections[index].title, // TITLE
+                        style: TextStyle(
+                          fontSize: 20,
+                          fontWeight: FontWeight.bold,
                         ),
                       ),
-                      Container(
-                        child: Text(
-                          DateFormat.yMMMEd().add_jm().format(tx.date),
-                          style: TextStyle(
-                            color: Colors.grey,
-                          ),
+                    ),
+                    Container(
+                      child: Text(
+                        DateFormat.yMMMEd()
+                            .add_jm()
+                            .format(_userTransections[index].date),
+                        style: TextStyle(
+                          color: Colors.grey,
                         ),
                       ),
-                    ],
-                  )
-                  //###############################################
-                  //-------------END OF TITLE AND DATE-------------
-                  //###############################################
-                ],
-              ),
-            );
-          }).toList(),
-        ),
+                    ),
+                  ],
+                )
+                //###############################################
+                //-------------END OF TITLE AND DATE-------------
+                //###############################################
+              ],
+            ),
+          );
+        },
+        itemCount: _userTransections.length,
       ),
     );
   }
